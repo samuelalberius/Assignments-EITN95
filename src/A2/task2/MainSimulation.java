d1785a15339fe5571ed6d7730cd0534c33cfe82f
jonathanr <jonisris@hotmail.com>
2022-05-04 14:10:08 +0200

    task 2 done

--- src/A2/task2/MainSimulation.java
+++ src/A2/task2/MainSimulation.java
@@ -7,6 +7,7 @@ public class MainSimulation extends GlobalSimulation {
 
 		int NRUNS = 1000;
 		ArrayList<Double> times = new ArrayList<Double>();
+		ArrayList<Double> aveServiceTimes = new ArrayList<Double>();
 
 		for (int i = 0; i < NRUNS; i++) {
 			time = 0; // set time = 0 start of simulation.
@@ -14,41 +15,55 @@ public class MainSimulation extends GlobalSimulation {
 			State actState = new State(); // The state that shoud be used
 			// Some events must be put in the event list at the beginning
 			insertEvent(ARRIVAL, 0);
-			insertEvent(MEASURE, 5);
+			insertEvent(MEASURE, 1);
 
-			// work while there are still people in queue or shift has ended
+			// work while there are either still people in queue or shift has ended
 			while (time < workTime || actState.numberInQueue > 0) {
 				actEvent = eventList.fetchEvent();
 				time = actEvent.eventTime;
 				actState.treatEvent(actEvent);
 			}
+			// add time at end of simulation
 			times.add(time);
+			aveServiceTimes.add(mean(actState.timesInSystem));
 		}
 		// Simulation has been run NRUNS times.
 
-		double totalTime = 0;
-		for (Double x : times) {
-			totalTime += x;
-		}
-		double meanTime = totalTime / NRUNS;
+		// confidence interval for average time it takes to fill out prescription
+		double meanServiceTime = mean(aveServiceTimes);
+		double stdServiceTime = std(aveServiceTimes, meanServiceTime);
+		double[] cIntervalServiceTime = { meanServiceTime - 1.96 * stdServiceTime/Math.sqrt(NRUNS),
+				meanServiceTime + 1.96 * stdServiceTime/Math.sqrt(NRUNS) };
 
-		double SSE = 0;
-		for (Double x : times) {
-			SSE += Math.pow((x - meanTime), 2);
-		}
-		double std = Math.sqrt(SSE / NRUNS);
-		// System.out.println(SSE);
-		double[] cInterval = { meanTime - 1.96 * std, meanTime + 1.96 * std };
+		System.out.println("Confidence interval (minutes): " + cIntervalServiceTime[0] + " - " + cIntervalServiceTime[1]);
+		System.out.println("Mean prescription filling time : " + meanServiceTime);
+
+		// confidence interval for total opening hours
+		double meanTime = mean(times);
+		double stdTime = std(times, meanTime);
+		double[] cIntervalTime = { meanTime - 1.96 * stdTime/Math.sqrt(NRUNS), meanTime + 1.96 * stdTime/Math.sqrt(NRUNS) };
 
 		int meanMinutes = (int) meanTime % 60;
 		int meanHours = (int) meanTime / 60;
 		System.out.println("Mean time work finished: " + (9 + meanHours) + ":" + meanMinutes);
-		System.out.println(String.format("Confidence interval: %d : %d - %d : %d", 9 + (int) cInterval[0] / 60,
-				(int) cInterval[0] % 60, 9 + (int) cInterval[1] / 60, (int) cInterval[1] % 60));
+		System.out.println(String.format("Confidence interval: %d : %d - %d : %d", 9 + (int) cIntervalTime[0] / 60,
+				(int) cIntervalTime[0] % 60, 9 + (int) cIntervalTime[1] / 60, (int) cIntervalTime[1] % 60));
 
-		// times.forEach( (n) -> { if (n < 481) {
-		// 	System.out.println(n); }} );
+	}
 
+	private static double mean(List<Double> list) {
+		double totalTime = 0;
+		for (Double x : list) {
+			totalTime += x;
+		}
+		return totalTime / list.size();
+	}
 
+	private static double std(List<Double> list, double mean) {
+		double SSE = 0;
+		for (Double x : list) {
+			SSE += Math.pow((x - mean), 2);
+		}
+		return Math.sqrt(SSE / list.size()); // std
 	}
 }
